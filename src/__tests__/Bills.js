ecf048006e36366eaba32c5f5f1addc2edbbad06
DVvor <85024600+DVvor@users.noreply.github.com>
2022-08-26 11:59:11 +0200

    verify class show is exist

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -17,17 +17,16 @@ import store from "../__mocks__/store";
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
-
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
-      }))
+      })) // Set as employee connected
       const root = document.createElement("div")
       root.setAttribute("id", "root")
       document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.Bills)
-      await waitFor(() => screen.getByTestId('icon-window'))
+      router() //
+      window.onNavigate(ROUTES_PATH.Bills) // Direction page bills
+      await waitFor(() => screen.getByTestId('icon-window')) 
       const windowIcon = screen.getByTestId('icon-window')
       //to-do write expect expression
       expect(windowIcon).toBeTruthy();
@@ -39,6 +38,7 @@ describe("Given I am connected as an employee", () => {
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
+
   })
   describe("When I clicked on eye icon of a bill", () => {
     test("should open a modal with a bill proof", () => {
@@ -47,18 +47,22 @@ describe("Given I am connected as an employee", () => {
       }
       const bill = new Bills({
         document, onNavigate, store: store, localStorage: window.localStorage
-      })
+      }) 
       
       const modaleFile = document.getElementById("modaleFile")
       expect(modaleFile).toBeTruthy()
       
-      $.fn.modal = jest.fn(() => modaleFile.classList.add("show"))
+      // Mock modal comportment
+      $.fn.modal = jest.fn(modaleFile.classList.add("show"));
+      
+      // Verify class "show" exist to modaleFile
       let classes = document.getElementById('modaleFile').getAttribute('class')
       classes = classes.split(' ')
-      expect(classes.includes('show'));
+      expect(classes.includes('show')).toBe(true) // 
 
       const handleClickIconEye = jest.fn((icon) => bill.handleClickIconEye(icon))
 
+      // function is called on each eyeicon clicked 
       const eyeIcon = screen.getAllByTestId('icon-eye')
       eyeIcon.forEach(icon => {
         icon.addEventListener("click", handleClickIconEye(icon))
@@ -75,15 +79,33 @@ describe("Given I am connected as an employee", () => {
       }
 
       const bill = new Bills({
-        document, onNavigate, store: null, localStorage: window.localStorage
-      })
+        document, onNavigate, store: store, localStorage: window.localStorage
+      }) //
       
       const handleClickNewBill = jest.fn(bill.handleClickNewBill)
       const buttonNewBill = screen.getByTestId('btn-new-bill')
       buttonNewBill.addEventListener('click', handleClickNewBill)
-      userEvent.click(buttonNewBill)
-      expect(handleClickNewBill).toHaveBeenCalled()
+      userEvent.click(buttonNewBill) // userEvent is a feature to simulate User. Here, user click on btn
+      expect(handleClickNewBill).toHaveBeenCalled() 
       expect(screen.getByText('Envoyer une note de frais')).toBeTruthy()
+    })
+  })
+})
+
+describe("Given I am a user connected as Employee", () => {
+  describe("When I navigate to bill page", () => {
+    test("Then it should fetches bills from mock API GET", async () => {
+      localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+
+      document.body.innerHTML = BillsUI({ data: bills }) // 
+      
+      await waitFor(() => screen.getByText("Mes notes de frais")) // wait after event 
+      expect(screen.getByText("Mes notes de frais")).toBeTruthy() // check text in screen
     })
   })
 })
